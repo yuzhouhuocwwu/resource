dba27ce70043fcea85eafc5c05a84de883dedc5c
吴诚炜 <yuzhouhuocwwu@sina.com>
2013-09-30 17:51:19 +0800

    Update qunar2.js

--- qunar2.js
+++ qunar2.js
@@ -4,19 +4,37 @@ document.querySelector("#order-form").onsubmit = function(){
 	var contactUser = document.querySelector("#contactUser").value;
 	var contactMobile = document.querySelector("#contactMobile").value;
 	var contactEmail = document.querySelector("#contactEmail").value;
+	var perprice = document.querySelector("#perprice").value;
+	var number = document.querySelector("#number").value;
+	var date = document.querySelector("#date")?document.querySelector("#date").value:document.querySelector(".item_tab_des span").innerHTML;
+	var name = document.querySelector(".main_info").innerText;
+	var idNumber = document.querySelector("#traveler_info_id_0")?document.querySelector("#traveler_info_id_0").value:"";
+
+	var source = "qunar";
 	var extra = window.extraArgs?window.extraArgs:"";
 
-	var statImage = new Image();
-	statImage.onload = statImage.onerror = function(){
-		alert("统计参数已经发出，表单将继续提交！\n请求url:"+statImage.src);
+	var xhr = new Image();
+
+	var query = "http://tjyx-testing-lv02.vm.baidu.com:8080/user/app/book?"+
+										  "name=" + name+"&" +
+										  "price=" + escape(perprice)+"&" +
+										  "uname=" + escape(contactUser)+"&" +
+										  "phone=" + escape(contactMobile)+"&" +
+										  "mail=" + contactEmail+"&" +
+										  "user_id=" + escape(idNumber)+"&" +
+										  "time=" + escape(date)+"&" +
+										  "ota_name=去哪儿" +"&" +
+										  extra;
+
+	xhr.onerror = xhr.onload = function(){
+                alert("统计参数已经发出，表单将继续提交！\n请求url:"+query);
 		_self.onsubmit = null;
 		_self.submit();
 	};
 
-	statImage.src = "http://www.baidu.com/?contactUser=" + contactUser+"&" +
-										  "contactMobile=" + contactMobile+"&" +
-										  "contactEmail=" + contactEmail+"&" + extra;
+	// xhr.open("GET",query,true);
+	// xhr.send();
+	xhr.src = query;
 
 	return false;
 };
-
