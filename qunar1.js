4b07e9fb85f2ef88a5a37e332ec62b7a137f4142
吴诚炜 <yuzhouhuocwwu@sina.com>
2013-10-23 13:55:27 +0800

    Create qunar1.js

---
+++ qunar1.js
@@ -0,0 +1,40 @@
+document.querySelector("#order-form").onsubmit = function(){
+	var _self = this;
+
+	var contactUser = document.querySelector("#contactUser").value;
+	var contactMobile = document.querySelector("#contactMobile").value;
+	var contactEmail = document.querySelector("#contactEmail").value;
+	var perprice = document.querySelector("#perprice").value;
+	var number = document.querySelector("#number").value;
+	var date = document.querySelector("#date")?document.querySelector("#date").value:document.querySelector(".item_tab_des span").innerHTML;
+	var name = document.querySelector(".main_info").innerText;
+	var idNumber = document.querySelector("#traveler_info_id_0")?document.querySelector("#traveler_info_id_0").value:"";
+
+	var source = "qunar";
+	var extra = window.extraArgs?window.extraArgs:"";
+
+	var xhr = new Image();
+
+	var query = "http://lvyou.baidu.com/user/app/book?"+
+										  "name=" + name+"&" +
+										  "price=" + escape(perprice)+"&" +
+										  "uname=" + escape(contactUser)+"&" +
+										  "phone=" + escape(contactMobile)+"&" +
+										  "mail=" + contactEmail+"&" +
+										  "user_id=" + escape(idNumber)+"&" +
+										  "time=" + escape(date)+"&" +
+										  "ota_name=去哪儿" +"&" +
+										  extra;
+
+	xhr.onerror = xhr.onload = function(){
+                alert("统计参数已经发出，表单将继续提交！\n请求url:"+query);
+		_self.onsubmit = null;
+		_self.submit();
+	};
+
+	// xhr.open("GET",query,true);
+	// xhr.send();
+	xhr.src = query;
+
+	return false;
+};
